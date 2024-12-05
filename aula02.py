11bf175399b7ab990582588845bb9b53413babf4
Juan Batista <168377692+yJuanz@users.noreply.github.com>
2024-12-05 11:24:47 -0300

    Início python
    declaração de variáveis

---
+++ aula02.py
@@ -0,0 +1,12 @@
+print('Bom dia!')
+
+nome_usuario = 'Juan' #string = texto
+idade = 20 #inteiro = int
+altura = 1.76 #double ou float 
+maior_idade = True
+
+print('Seja bem vindo', nome_usuario)
+print('Você tem', idade, 'anos')
+print('Sua altura é', altura)
+print('Você é maior de idade?', maior_idade)
+
